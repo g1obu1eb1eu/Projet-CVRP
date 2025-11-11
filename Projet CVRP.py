47e069d04003005647b28f9ebbf4253814d123fb
g1obu1eb1eu <147941528+g1obu1eb1eu@users.noreply.github.com>
2025-11-11 15:57:50 +0100

    Graph modelisation & modif of settings - Modification of the settings to accept linux, macOs env - Finalization of the graphic representation of data

--- Projet CVRP.py
+++ Projet CVRP.py
@@ -9,8 +9,15 @@ from pathlib import Path
 path_to_instances = Path(__file__).parent / "full_dataset"
 
 # Path to specific instance & solution files
-instance_file = path_to_instances / "X-n101-k25.vrp"
-solution_file = path_to_instances / "X-n101-k25.sol"
+
+# Dataset 1
+instance_file = path_to_instances / "A-n32-k5.vrp"
+solution_file = path_to_instances / "A-n32-k5.sol"
+
+# Dataset 2
+# instance_file = path_to_instances / "X-n101-k25.vrp"
+# solution_file = path_to_instances / "X-n101-k25.sol"
+
 
 if not instance_file:
     raise FileNotFoundError(f"Instance file not found at : {instance_file}")
@@ -31,36 +38,49 @@ solution = vrplib.read_solution(solution_file)
 # for key, val in solution_items:
 #     print(f"{key} : {val}\n")
 
-# print(f"{instance["edge_weight"]}")
+# print(f"{instance['edge_weight']}")
 
-x_coord = [x for x, y in instance["node_coord"]]
-y_coord = [y for x, y in instance["node_coord"]]
+def graph():
+    dimension = randint(3, 10)
+    my_graph = [[0 for _ in range(dimension)] for _ in range(dimension)]
 
-plt.scatter(x_coord, y_coord, edgecolors="red")
-plt.plot(x_coord, y_coord, "-o")
+    for i in range(dimension):
+        for k in range(i + 1, dimension):
+            val = randint(1, 20)
+            my_graph[i][k], my_graph[k][i] = val, val
 
-plt.show()
-
-# def graph():
-#     dimension = randint(3, 10)
-#     my_graph = [[0 for _ in range(dimension)] for _ in range(dimension)]
-
-#     for i in range(dimension):
-#         for k in range(i + 1, dimension):
-#             val = randint(1, 20)
-#             my_graph[i][k], my_graph[k][i] = val, val
-
-#     return my_graph
+    return my_graph
 
 
-def show_graph(sac):
-    rows, cols = np.where(sac)
-    edges = zip(list(rows), list(cols))
+def show_graph(edge_weight, node_coord):
     gr = nx.Graph()
+    num_nodes = len(node_coord)
+    edges = []
+    for i in range(num_nodes):
+        for j in range(num_nodes):
+            if i != j:
+                w = float(edge_weight[i, j])
+                if w > 0:
+                    edges.append((i, j, {"weight": w}))
+
     gr.add_edges_from(edges)
-    nx.draw(gr, node_size=500)
+
+    # pos = {point: point for point in node_coord}
+
+    pos = {
+        i: (float(node_coord[i][0]), float(node_coord[i][1]))
+        for i in range(len(node_coord))
+    }
+    # print(pos)
+
+    fig, ax = plt.subplots()
+    nx.draw(gr, pos=pos, node_size=500, node_color="skyblue", edge_color="black", ax=ax)
+    # nx.draw_networkx_labels(gr, pos=pos)
+    # labels = nx.get_edge_attributes(gr, "weight")
+    # nx.draw_networkx_edge_labels(gr, pos, edge_labels=labels, ax=ax)
+    plt.axis("on")
+    ax.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
     plt.show()
 
 
-# sac = graph()
-show_graph(instance["edge_weight"])
+show_graph(instance["edge_weight"], instance["node_coord"])
